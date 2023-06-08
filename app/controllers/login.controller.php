850823ee4a84672c6e5bf1370c54caf67fd7f3bc
OstertagJP <figuta@gmail.com>
2023-06-08 19:16:03 -0300

    controller.php terminado

--- app/controllers/login.controller.php
+++ app/controllers/login.controller.php
@@ -25,3 +25,22 @@ function login(){
 
     $this->view-> mostrar();
 }
+
+//Tercera parte
+
+function home(){
+    $user= $_POST["user"];
+    $pass=$_POST["password"];
+    $usuario=$this->model->get_usuarios($user);
+   
+     if($usuario->name==$user && password_verify($pass,$usuario->password)){
+         $this->view->mostrar_home($user);
+     
+     }else{
+     
+         $this->view-> mostrar("Usuario o contraseña incorrecta");
+    }
+ }
+
+    
+
